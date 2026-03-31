f4eeec39c9f079c7a9ab04c764105258d73c3302
Rakesh Yadav <rakeshydv046@gmail.com>
2026-03-31 14:50:52 +0545

    code change

---
+++ emplStatus.py
@@ -0,0 +1,111 @@
+import streamlit as st
+import requests
+API_URL= 'https://fast-api-d4fg.onrender.com/predict-status'
+
+st.header("Employee Termination or Active Prediction using Logistic Regression.")
+st.subheader("hek")
+
+st.sidebar.header(
+    "Employee Status Prediction"
+)
+
+PerfScoreID =st.sidebar.slider(
+    "Performance Score",
+    value = 3,
+    min_value=0,
+    max_value=5,
+    step=1
+
+)
+SalaryID= st.sidebar.slider(
+    "Salary",
+    value=10000,
+    min_value=1000,
+    max_value=600000,
+    step=10
+)
+PositionID= st.sidebar.slider(
+    "PositionID",
+    value=5,
+    min_value=0,
+    max_value=30,
+    step=1
+)
+EngagementSurvey= st.sidebar.slider(
+    "Engagement Survey",
+    value=5.0,
+    min_value=0.0,
+    max_value=30.0,
+    step=0.1
+)
+EmpSatisfaction= st.sidebar.slider(
+    "Emp Satisfaction",
+    value=3,
+    min_value=0,
+    max_value=5,
+    step=1
+)
+SpecialProjectsCount= st.sidebar.slider(
+    "Special Projects Count'",
+    value=5,
+    min_value=1,
+    max_value=30,
+    step=1
+)
+DaysLateLast30= st.sidebar.slider(
+    "Late Days",
+    value=5,
+    min_value=0,
+    max_value=30,
+    step=1
+)
+Absences= st.sidebar.slider(
+    "Absences",
+    value=5,
+    min_value=0,
+    max_value=30,
+    step=1
+)
+if st.button("Predict Status"):
+    payload={
+        "PerfScoreID":PerfScoreID,
+        "SalaryID":SalaryID,
+        "PositionID":PositionID,
+        "EngagementSurvey":EngagementSurvey,
+        "EmpSatisfaction":EmpSatisfaction,
+        "SpecialProjectsCount":SpecialProjectsCount,
+        "DaysLateLast30":DaysLateLast30,
+        "Absences":Absences
+        
+    }
+    try:
+        response=requests.post(AP_URL,payload)
+        if response.status_code==200:
+            result = response.json()
+
+            if result["Predicted_Status"]==1:
+                st.success("Employee likely to be active")
+            else:
+                st.warning("Employee likely to be Terminated in Future.Kam na Lagne Manxe")
+        else:
+            st.error("Path is Invalid!")
+    except requests.exceptions.RequestException:
+        st.error("Could not connect to API!!")
+    # input_data=pd.DataFrame([[
+    #     PerfScoreID,SalaryID,PositionID,EngagementSurvey,EmpSatisfaction,SpecialProjectsCount,DaysLateLast30,Absences
+    # ]],columns=features)
+    # input_scaler=scaler.transform(input_data)
+    # prediction=model.predict(input_scaler)
+
+    # if prediction[0]==0:
+    #     st.success("Employee is likely to be active in future 👍👌😊")
+    #     result='Active'
+    # else:
+    #     st.warning("Employee is likely to be terminated in futre..")
+    #     result='Terminated'
+    # st.metric(
+    #     "Employee Status",
+    #     value=result
+    # )
+
+    
